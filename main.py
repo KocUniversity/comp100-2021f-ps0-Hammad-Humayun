85095a163683e805956daf815080044dcc8e6e54
Hammad-Humayun <91727171+Hammad-Humayun@users.noreply.github.com>
2021-10-01 10:12:07 +0000

    This is my first assignment, student id: 0078977

---
+++ main.py
@@ -0,0 +1,7 @@
+import numpy
+x= int(input("enter a number x : "))
+y= int(input("enter a number y : "))
+print("x**y= ", x**y) 
+print("log(base 2)of x = ", numpy.log2(x))
+print("Student id: 0078977")
+
